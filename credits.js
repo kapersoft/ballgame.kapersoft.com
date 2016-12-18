947ce047df45badffb0cb89ab603d1ca06e05834
Jan Willem Kaper <kapersoft@gmail.com>
2016-12-18 01:03:32 +0100

    Removed some redundant  parentheses

--- credits.js
+++ credits.js
@@ -41,10 +41,10 @@ Credits = function() {
         var x = mouseX || touches[touches.length -1].x;
         var y = mouseY || touches[touches.length -1].y;
 
-        if ((x >= (this.x)) &&
-            (x <= (this.x + this.width)) &&
-            (y >= (this.y + this.height * 0.45) &&
-            (y <= (this.y + this.height)))) {
+        if (x >= this.x &&
+            x <= this.x + this.width &&
+            y >= this.y + this.height * 0.45 &&
+            y <= this.y + this.height) {
             open("https://github.com/kapersoft/ballgame.kapersoft.com/");
         }
         else {
